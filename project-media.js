7b389f0a99c36c0c85e6b97310f4d416f945800f
nox9112 <strohm.christian@web.de>
2026-08-26 01:24:00 +0200

    Add compact product photo references for pedal library

---
+++ project-media.js
@@ -0,0 +1,30 @@
+(()=>{
+'use strict';
+window.ToneForgeMedia={
+  'pedal:fuzz-face':{
+    photo:'https://thumbs.static-thomann.de/thumb//bdbmagic/pics/prod/309741.jpg',
+    source:'https://www.thomann.de/de/dunlop_germanium_fuzz_face_mini_red.htm',
+    credit:'Thomann · Dunlop Germanium Fuzz Face Mini'
+  },
+  'pedal:tube-screamer':{
+    photo:'https://thumbs.static-thomann.de/thumb/padthumb600x600/pics/bdb/_17/170627/10631407_800.jpg',
+    source:'https://www.thomannmusic.com/ibanez_ts808.htm',
+    credit:'Thomann · Ibanez TS808'
+  },
+  'pedal:rat':{
+    photo:'https://tonearchitect.app/proco-rat2-photo.jpg',
+    source:'https://tonearchitect.app/',
+    credit:'Tone Architect · ProCo RAT 2'
+  },
+  'pedal:micro-amp':{
+    photo:'https://cdn11.bigcommerce.com/s-n26aknlnlm/images/stencil/1280x1280/products/2064/10730/M133.MAIN__39765.1701991028.jpg?c=2',
+    source:'https://www.jimdunlop.com/mxr-micro-amp-parts/',
+    credit:'Jim Dunlop · MXR Micro Amp'
+  },
+  'pedal:phase90':{
+    photo:'https://www.chicagomusicexchange.com/cdn/shop/files/media_ffcdee79-314f-4d61-a99f-6d1e277224b8.jpg?v=1719419514',
+    source:'https://www.chicagomusicexchange.com/products/mxr-phase-90-2071063',
+    credit:'Chicago Music Exchange · MXR Phase 90'
+  }
+};
+})();
