c8170d81d1be04dbf5d9364d5ec875eb5bbbaf1f
nox9112 <strohm.christian@web.de>
2026-08-26 01:31:42 +0200

    Expand compact pedal photo references

--- project-media.js
+++ project-media.js
@@ -1,30 +1,15 @@
 (()=>{
 'use strict';
 window.ToneForgeMedia={
-  'pedal:fuzz-face':{
-    photo:'https://thumbs.static-thomann.de/thumb//bdbmagic/pics/prod/309741.jpg',
-    source:'https://www.thomann.de/de/dunlop_germanium_fuzz_face_mini_red.htm',
-    credit:'Thomann · Dunlop Germanium Fuzz Face Mini'
-  },
-  'pedal:tube-screamer':{
-    photo:'https://thumbs.static-thomann.de/thumb/padthumb600x600/pics/bdb/_17/170627/10631407_800.jpg',
-    source:'https://www.thomannmusic.com/ibanez_ts808.htm',
-    credit:'Thomann · Ibanez TS808'
-  },
-  'pedal:rat':{
-    photo:'https://tonearchitect.app/proco-rat2-photo.jpg',
-    source:'https://tonearchitect.app/',
-    credit:'Tone Architect · ProCo RAT 2'
-  },
-  'pedal:micro-amp':{
-    photo:'https://cdn11.bigcommerce.com/s-n26aknlnlm/images/stencil/1280x1280/products/2064/10730/M133.MAIN__39765.1701991028.jpg?c=2',
-    source:'https://www.jimdunlop.com/mxr-micro-amp-parts/',
-    credit:'Jim Dunlop · MXR Micro Amp'
-  },
-  'pedal:phase90':{
-    photo:'https://www.chicagomusicexchange.com/cdn/shop/files/media_ffcdee79-314f-4d61-a99f-6d1e277224b8.jpg?v=1719419514',
-    source:'https://www.chicagomusicexchange.com/products/mxr-phase-90-2071063',
-    credit:'Chicago Music Exchange · MXR Phase 90'
-  }
+  'pedal:fuzz-face':{photo:'https://thumbs.static-thomann.de/thumb//bdbmagic/pics/prod/309741.jpg',source:'https://www.thomann.de/de/dunlop_germanium_fuzz_face_mini_red.htm',credit:'Thomann · Dunlop Germanium Fuzz Face Mini'},
+  'pedal:tube-screamer':{photo:'https://thumbs.static-thomann.de/thumb/padthumb600x600/pics/bdb/_17/170627/10631407_800.jpg',source:'https://www.thomannmusic.com/ibanez_ts808.htm',credit:'Thomann · Ibanez TS808'},
+  'pedal:rat':{photo:'https://tonearchitect.app/proco-rat2-photo.jpg',source:'https://tonearchitect.app/',credit:'Tone Architect · ProCo RAT 2'},
+  'pedal:big-muff':{photo:'https://cdn.shopify.com/s/files/1/0618/9039/6265/files/electro-harmonix-big-muff-usa-music-planet-new-zealand-6712459_400x400.jpg?v=1752168771',source:'https://www.musicplanet.co.nz/products/electro-harmonix-usa-big-muff-pi-distortion-sustainer-ehx-bm',credit:'Music Planet · Electro-Harmonix Big Muff Pi'},
+  'pedal:compressor':{photo:'https://thumbs.static-thomann.de/thumb//bdbmagic/pics/prod/129745.jpg',source:'https://www.thomann.de/de/mxr_dynacomp.htm',credit:'Thomann · MXR Dyna Comp'},
+  'pedal:phase90':{photo:'https://www.chicagomusicexchange.com/cdn/shop/files/media_ffcdee79-314f-4d61-a99f-6d1e277224b8.jpg?v=1719419514',source:'https://www.chicagomusicexchange.com/products/mxr-phase-90-2071063',credit:'Chicago Music Exchange · MXR Phase 90'},
+  'pedal:chorus':{photo:'https://monteallums.com/wordpress/wp-content/uploads/2023/09/CE-2_Web.jpg',source:'https://monteallums.com/wordpress/product/boss-ce-2-ce-2b-diy-pedal-mod-kits/',credit:'Monte Allums · Boss CE-2 reference'},
+  'pedal:distortion-plus':{photo:'https://www.truetonemusic.com/cdn/shop/files/1-Img5904_700x700.jpg?v=1713816743',source:'https://www.truetonemusic.com/products/used-mxr-distortion-plus-pedal-with-box',credit:'Truetone Music · MXR Distortion+'},
+  'pedal:vox-v847':{photo:'https://cdn.sanity.io/images/5skqgt41/production/2a6d029d229e836ab8fe8f7ae8ee86e5f8b51d4b-1000x1000.jpg?auto=format',source:'https://www.mannys.com.au/products/vox-v847-wah-pedal',credit:'Mannys · Vox V847'},
+  'pedal:micro-amp':{photo:'https://cdn11.bigcommerce.com/s-n26aknlnlm/images/stencil/1280x1280/products/2064/10730/M133.MAIN__39765.1701991028.jpg?c=2',source:'https://www.jimdunlop.com/mxr-micro-amp-parts/',credit:'Jim Dunlop · MXR Micro Amp'}
 };
 })();
