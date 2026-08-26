cc4edb0cbd1d9c9bed14f31e4a165fad3d16aa4a
nox9112 <strohm.christian@web.de>
2026-08-26 21:45:45 +0200

    Add stable official media for four ToneForge projects

--- project-media.js
+++ project-media.js
@@ -8,10 +8,14 @@ window.ToneForgeMedia={
   'pedal:compressor':{photo:'https://thumbs.static-thomann.de/thumb//bdbmagic/pics/prod/129745.jpg',source:'https://www.thomann.de/de/mxr_dynacomp.htm',credit:'Thomann · MXR Dyna Comp'},
   'pedal:phase90':{photo:'https://cdn11.bigcommerce.com/s-n26aknlnlm/images/stencil/500x659/products/691/6739/16101003001.MAIN__44494.1663874799.jpg?c=2',source:'https://www.jimdunlop.com/mxr-74-vintage-phase-90/',credit:'Jim Dunlop · MXR ’74 Vintage Phase 90'},
   'pedal:chorus':{photo:'https://monteallums.com/wordpress/wp-content/uploads/2023/09/CE-2_Web.jpg',source:'https://monteallums.com/wordpress/product/boss-ce-2-ce-2b-diy-pedal-mod-kits/',credit:'Monte Allums · Boss CE-2 reference'},
+  'pedal:pt2399':{photo:'https://store.synthrotek.com/assets/images/PT2399_Dev_Delay_Complete_01.jpg',source:'https://store.synthrotek.com/PT2399_Dev_Delay_Kit',credit:'Synthrotek · PT2399 Dev Delay Kit'},
   'pedal:distortion-plus':{photo:'https://www.truetonemusic.com/cdn/shop/files/1-Img5904_700x700.jpg?v=1713816743',source:'https://www.truetonemusic.com/products/used-mxr-distortion-plus-pedal-with-box',credit:'Truetone Music · MXR Distortion+'},
   'pedal:vox-v847':{photo:'https://cdn.sanity.io/images/5skqgt41/production/2a6d029d229e836ab8fe8f7ae8ee86e5f8b51d4b-1000x1000.jpg?auto=format',source:'https://www.mannys.com.au/products/vox-v847-wah-pedal',credit:'Mannys · Vox V847'},
   'pedal:boss-ds1':{photo:'https://static.roland.com/products/ds-1/images/ds-1_top.png',source:'https://www.boss.info/GLOBAL/products/ds-1/',credit:'BOSS · DS-1 Distortion'},
   'pedal:sd1':{photo:'https://static.roland.com/products/sd-1-4a/images/sd-1_top.png',source:'https://www.boss.info/de/products/sd-1/',credit:'BOSS · SD-1 Super OverDrive'},
-  'pedal:micro-amp':{photo:'https://cdn11.bigcommerce.com/s-n26aknlnlm/images/stencil/1280x1280/products/2064/10730/M133.MAIN__39765.1701991028.jpg?c=2',source:'https://www.jimdunlop.com/mxr-micro-amp-parts/',credit:'Jim Dunlop · MXR Micro Amp'}
+  'pedal:micro-amp':{photo:'https://cdn11.bigcommerce.com/s-n26aknlnlm/images/stencil/1280x1280/products/2064/10730/M133.MAIN__39765.1701991028.jpg?c=2',source:'https://www.jimdunlop.com/mxr-micro-amp-parts/',credit:'Jim Dunlop · MXR Micro Amp'},
+  'pedal:volume-passive':{photo:'https://s3.us-west-2.amazonaws.com/static.ernieball.com/website/images/products/image_front/large/P06180.png',source:'https://www.ernieball.com/guitar-accessories/pedals/volume-pedals',credit:'Ernie Ball · VP Jr 250K P06180'},
+  'pedal:sample-trigger':{photo:'https://cdn-shop.adafruit.com/970x728/2220-06.jpg',source:'https://www.adafruit.com/product/2220',credit:'Adafruit · Audio FX Sound Board 16MB'},
+  'pedal:talkbox':{photo:'https://cdn11.bigcommerce.com/s-n26aknlnlm/images/stencil/500x659/products/656/6580/11501000001.MAIN__54495.1663874797.jpg?c=2',source:'https://www.jimdunlop.com/heil-talk-box/',credit:'Jim Dunlop / Heil · HT-1 Talk Box'}
 };
 })();
